d6587c2601986f3f8d7ab35a15eb4015de3b08bf
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-08-01 11:51:01 -0300

    readme
    readme

--- bootProjet/src/bootProjet/AppCarro.java
+++ bootProjet/src/bootProjet/AppCarro.java
@@ -1,5 +1,24 @@
 package bootProjet;
 
+import java.util.Scanner;
+import entidade.Carro;
+
 public class AppCarro {
+	
+	public static void main(String[] args) {
+		
+		Scanner tec = new Scanner(System.in);
+		
+		Carro car1 = new Carro();
+		
+		
+		car1.setCor = tec.nextLine();
+		car1. = tec.nextLine();
+		
+		
+		
+		tec.close();
+		
+	}
 
 }
--- bootProjet/src/entidade/Carro.java
+++ bootProjet/src/entidade/Carro.java
@@ -1,5 +1,76 @@
 package entidade;
 
+
 public class Carro {
+	
+	private String cor;
+	private String modelo;
+	private float capacidadeDoTanque;
+	
+	
+	public Carro() {
+    }
+
+	public Carro(String cor, String modelo, float capacidadeDoTanque) {
+		this.cor = cor;
+		this.modelo = modelo;
+		this.capacidadeDoTanque = capacidadeDoTanque;
+	}
+	
+	public Carro( String modelo, float capacidadeDoTanque) {
+		this.modelo = modelo;
+		this.capacidadeDoTanque = capacidadeDoTanque;
+	}
+
+
+	public String getCor() {
+		return cor;
+	}
+
+
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
+
+	public float getCapacidadeDoTanque() {
+		return capacidadeDoTanque;
+	}
+
+
+
+	public void setCapacidadeDoTanque(float capacidadeDoTanque) {
+		this.capacidadeDoTanque = capacidadeDoTanque;
+	}
+	
+	public float abastecimento(float valorDoCombustivel) {
+		return capacidadeDoTanque * valorDoCombustivel;
+	}
+	
+	
+	
+	public String toString() {
+		return ""
+	}
+	
+	
+	
+	
+	
+	
 
 }
